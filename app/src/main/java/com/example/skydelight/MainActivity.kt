952e9cae22be87a2d48d068f11d223a75ab1b96e
Santi PerGo <assasinsky@hotmail.com>
2022-07-25 13:24:18 -0500

    Primer Commit con Diseño de Pantallas XML

---
+++ app/src/main/java/com/example/skydelight/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.skydelight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //hide action bar
+        supportActionBar?.hide()
+    }
+}
