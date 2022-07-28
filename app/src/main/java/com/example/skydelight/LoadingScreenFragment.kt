f31fe84bc4a80a49d4dea3df5aec2b25974e486d
Santi PerGo <assasinsky@hotmail.com>
2022-07-27 20:58:58 -0500

    Implementando ViewBinding y DataBinding

--- app/src/main/java/com/example/skydelight/LoadingScreenFragment.kt
+++ app/src/main/java/com/example/skydelight/LoadingScreenFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class LoadingScreenFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+    // Creating the fragment view
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_loading_screen, container, false)
     }
+
+    // After the view is created we can do things
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Changing to the start screen fragment
+        findNavController().navigate(R.id.action_loadingScreen_to_startScreen)
+    }
+
 }
